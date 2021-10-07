6d76ecd970707a99fc0980ae0c03a5ed63365b33
jridexx <60946816+jridexx@users.noreply.github.com>
2021-10-07 10:21:42 -0400

    Update dih.js

--- dih.js
+++ dih.js
@@ -1,1 +1,9 @@
-
+// Show references if there's references
+window.addEventListener("load", function(){
+  
+  const references = document.getElementsByClassName("references")[0];
+  if (references != undefined && references.children[1].childElementCount > 0) {
+    references.style.display="block";
+  }
+  
+});
