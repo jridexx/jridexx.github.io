c32ac9ae96d1c0f91ef621c97c3f75919601b748
jridexx <60946816+jridexx@users.noreply.github.com>
2022-03-24 13:56:53 -0400

    Update dih.js

--- dih.js
+++ dih.js
@@ -1,9 +1,16 @@
-// Show references if there's references
-window.addEventListener("load", function(){
+// A function to show references if there's references (commented out)
+
+/* window.addEventListener("load", function(){
   
   const references = document.getElementsByClassName("references")[0];
   if (references != undefined && references.children[1].childElementCount > 0) {
     references.style.display="block";
   }
   
+});*/
+
+//A function to ignore certain references
+
+window.addEventListener("load", function(){
+  document.querySelectorAll(".references a").forEach(x => x.children[0].classList.add("roam-block"));
 });
