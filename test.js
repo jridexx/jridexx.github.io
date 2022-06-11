458eef647a3e20bdb97b8c8be918f1a4adb23e76
jridexx <60946816+jridexx@users.noreply.github.com>
2022-06-11 15:39:21 -0400

    Update test.js

--- test.js
+++ test.js
@@ -1,1 +1,23 @@
+// Remove the transition class
+  const block = document.querySelector('.roam-block');
+  block.classList.remove('block-transition');
+  
+    // Create the observer
+    const observer = new IntersectionObserver(entries => {
 
+      entries.forEach(entry => {
+        
+              if (entry.isIntersecting) {
+                
+                // It's visible. Add the animation class here!
+                block.classList.add('block-transition');
+                
+              }
+
+              block.classList.remove('block-transition');
+      });
+      
+    });
+    
+    // Tell the observer which elements to track
+    observer.observe(document.querySelector('.roam-block'));
