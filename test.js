9db5360167bcf90c196bab07c1b2031002befd94
jridexx <60946816+jridexx@users.noreply.github.com>
2022-06-14 10:59:59 -0400

    Update test.js

--- test.js
+++ test.js
@@ -1,18 +1,26 @@
-const io = new IntersectionObserver(entries => {
-  entries.forEach(entry => {
-    // Add 'active' class if observation target is inside viewport
-    if (entry.intersectionRatio > 0) {
-      entry.target.classList.add('block-transition');
-    }
-    // Remove 'active' class otherwise
-    else {
-      entry.target.classList.remove('block-transition');
-    }
-  })
-})
+// Create IntersectionObserver object to monitor viewport
 
-// Declares what to observe, and observes its properties.
-const boxElList = document.querySelectorAll('.roam-block');
-boxElList.forEach((el) => {
-  io.observe(el);
-}) 
+    const io = new IntersectionObserver(entries => {
+      
+      entries.forEach(entry => {
+        
+        // Add 'active' class (.block-transition) if observation target is inside viewport
+        
+        if (entry.intersectionRatio > 0) {
+          
+          entry.target.classList.add('block-transition');
+          
+        }
+      });
+    });
+
+// On Load function
+    
+    window.addEventListener("load", function(){
+      
+      document.querySelectorAll('.roam-block').forEach((el) => {
+        
+        io.observe(el);
+        
+        });
+    });
