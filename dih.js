ad2fcf566c0ef0f0b31f1ee68f69c9aaf91916ea
jridexx <60946816+jridexx@users.noreply.github.com>
2023-01-15 13:02:07 -0500

    Update dih.js

--- dih.js
+++ dih.js
@@ -1,13 +1,13 @@
-// A function to show references if there's references (commented out)
+// A function to show references if there's references
 
-/* window.addEventListener("load", function(){
+window.addEventListener("load", function(){
   
   const references = document.getElementsByClassName("references")[0];
   if (references != undefined && references.children[1].childElementCount > 0) {
     references.style.display="block";
   }
   
-});*/
+});
 
 //A function to add a class by selector
 
